7fb08c8a8e318c6d705e6662af6c1cae6e40512f
zobaerahmedzihad1 <zobaerahmedzihad02@gmail.com>
2022-03-01 02:35:36 +0600

    showing all sensors

--- mobile-hunter.js
+++ mobile-hunter.js
@@ -9,28 +9,24 @@ const searchPhones = () => {
     .then((response) => response.json())
     .then((data) => displayPhones(data.data));
 };
-
 // Display all phones
 const displayPhones = (phones) => {
   phones.forEach((phone) => {
     //  console.log(phone);
-
     const { image, phone_name, slug, brand } = phone;
-
     const Phones = document.getElementById("phones");
-
     const div = document.createElement("div");
     div.classList.add("col");
     div.innerHTML = `
           <div class="card h-100">
                   <img src="${image}" class="card-img-top" alt="...">
-               <div class="card-footer badge bg-danger">
+               <div class="card-footer badge  text-dark mt-1">
                    <h5 class="card-title text-center text-uppercase">${phone_name}</h5>
                </div>
-               <div class="card-footer badge bg-info mt-1">
+               <div class="card-footer badge bg-secondary mt-1 mb-1">
                    <h5 class="card-title text-center text-uppercase">${brand}</h5>
                </div>
-               <div class="card-footer badge bg-light">
+               <div class="card-footer badge">
                     <button onclick = "showPhoneDetails('${slug}')" type="button" class="btn btn-outline-primary">Show Details</button>
                </div>
           </div>
@@ -52,15 +48,8 @@ const displayPhonesDetails = (phone) => {
   console.log(phone);
   const { image, brand, name } = phone;
 
+  // destucturing mainFeatures
   const { chipSet, displaySize, memory, sensors } = phone.mainFeatures;
-  console.log(sensors);
-
-  sensors.forEach((sensor) => {
-//     console.log(sensor);
-
-
-
-  });
 
   const phoneDetails = document.getElementById("phone-details");
   //   empty phone details
@@ -74,15 +63,31 @@ const displayPhonesDetails = (phone) => {
          <h5 class="card-text p-1 mt-2 border border-2 text-center rounded border-info">${name} </p5>
          <h5 class="card-text p-1 border border-2 text-center rounded border-info">${brand} </p5>
        </div>
+ 
        <div class="col-md-4">
          <div class="card-body">
            <P class="card-title"> 1. CHIPSET : ${chipSet}</P>
            <P class="card-title"> 2. MEMORY : ${memory}</P>
            <p class="card-text"> 3. DISPLAY SIZE : ${displaySize}</p>
          </div>
-         
        </div>
+       
+       <div id="sensor-container" class="card-body col-md-4">
+         
+        </div>
      </div>
+     
   `;
   phoneDetails.appendChild(div);
+
+  // sensor
+  const allSensor = phone.mainFeatures.sensors;
+  console.log(allSensor);
+  let sensorDiv = document.getElementById("sensor-container");
+
+  for (const sensor of allSensor) {
+    let sensorText = document.createElement("li");
+    sensorText.innerText = `${sensor}`;
+    sensorDiv.appendChild(sensorText);
+  }
 };
